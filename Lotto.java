7d2ee4de4c3fa528a2d4a2e617f023ff66a6aeb5
Angelica <Angelica137@users.noreply.github.com>
2021-03-15 19:04:18 +0000

    print one number

--- Lotto.java
+++ Lotto.java
@@ -9,12 +9,18 @@
 public class Lotto {
 
 	// Put the luckyDip() method here
+	public void luckyDip() {
+		int random;
+		int i = 1;
+		random = (int) (Math.random() * 49) + 1;
+		System.out.println("Ball " + i + random);
+	}
 
 	// Test
 	public static void main(String args[]) {
 
 		Lotto l = new Lotto();
-		// l.luckyDip();
+		l.luckyDip();
 	}
 
 }
