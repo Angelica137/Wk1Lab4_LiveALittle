95145bb8bb6c88705282960e84b705f4b4441efa
Angelica <Angelica137@users.noreply.github.com>
2021-03-15 18:57:21 +0000

    describe Lotto class

--- Lotto.java
+++ Lotto.java
@@ -1,28 +1,20 @@
 
 /**
- * Write a description of class Lotto here.
+ * Write a description of class Lotto here. Lotto generates a lucky dip and
+ * displays the lucky dip
  * 
- * @author (your name) 
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class Lotto
-{
-	
-	//Put the luckyDip() method here
-	
-	
+public class Lotto {
 
+	// Put the luckyDip() method here
 
+	// Test
+	public static void main(String args[]) {
 
-
-
-
-        //Test
-	public static void main (String args[]) {
-		
-		Lotto l=new Lotto();
-		//l.luckyDip();
+		Lotto l = new Lotto();
+		// l.luckyDip();
 	}
 
-	
 }
